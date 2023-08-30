49ec9b4a3d5d5b09fde9bc75def881306dac9765
ordila <ordila99@gmail.com>
2023-08-30 23:19:24 +0300

    hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  listElement: document.querySelector('.gallery'),
+};
+
+console.log(refs.listElement);
+
+function galleryMarkup(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+  
+      `;
+    })
+    .join('');
+  return markup;
+}
+
+function renderGallery(galleryItems) {
+  const markup = galleryMarkup(galleryItems);
+  refs.listElement.insertAdjacentHTML('beforeend', markup);
+}
+renderGallery(galleryItems);
+
+refs.listElement.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const imageUrlModal = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+        <img src='${imageUrlModal}'>
+    `);
+
+  instance.show();
+
+  document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+    }
+  });
+});
